0aa75f86e51cab0476b4f5b0d56988364969793b
William-Brown5515 <william.brown@stfc.ac.uk>
2025-04-24 12:32:32 +0100

    Add product class with constuctor

---
+++ src/Product.java
@@ -0,0 +1,15 @@
+public class Product {
+
+    private String name;
+    private String productID;
+    private int quantity;
+    private double price;
+
+    public Product(String name, String productID, int quantity, double price) {
+        this.name = name;
+        this.productID = productID;
+        this.quantity = quantity;
+        this.price = price;
+    }
+
+}
