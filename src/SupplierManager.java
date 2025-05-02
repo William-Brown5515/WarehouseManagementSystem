41428f7d84379c0ede577cd0c131f1f4dfdc56ad
William-Brown5515 <william.brown@stfc.ac.uk>
2025-05-02 11:05:47 +0100

    Add SupplierManager commenting

--- src/SupplierManager.java
+++ src/SupplierManager.java
@@ -1,17 +1,21 @@
 import java.util.ArrayList;
 import java.util.List;
 
+// A class to manage the suppliers
 public class SupplierManager {
     List<Supplier> suppliers;
 
+    // The constructor, to initialise the supplier list
     public SupplierManager() {
         this.suppliers = new ArrayList<Supplier>();
     }
 
+    // A method to add a new supplier
     public void addSupplier(Supplier supplier) {
         this.suppliers.add(supplier);
     }
 
+    // A method to remove a supplier by their ID
     public boolean removeSupplier(String supplierID) {
         for (Supplier supplier : this.suppliers) {
             if (supplier.getSupplierID().equals(supplierID)) {
@@ -22,6 +26,7 @@ public class SupplierManager {
         return false;
     }
 
+    // A method to return a supplier that matches the given ID
     public Supplier getSupplier(String supplierID) {
         for (Supplier supplier : this.suppliers) {
             if (supplier.getSupplierID().equals(supplierID)) {
@@ -31,6 +36,7 @@ public class SupplierManager {
         return null;
     }
 
+    // A method to print all suppliers
     public void displaySuppliers() {
         for (Supplier supplier : this.suppliers) {
             System.out.println(supplier.toString());
