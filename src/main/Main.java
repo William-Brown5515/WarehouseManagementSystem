771ec2f025cc33930557cf2cd6bbe741cd813a3b
William-Brown5515 <william.brown@stfc.ac.uk>
2025-05-16 10:43:32 +0100

    Correct order delivery delay and repeat ordering

--- src/main/Main.java
+++ src/main/Main.java
@@ -203,60 +203,61 @@ public class Main {
     // === Owner Actions ===
     private static void orderFromSuppliers() {
         supplierManager.displaySuppliers();
-        while (true) {
-            System.out.println("\n--- Supplier Order Menu ---");
-            System.out.println("Enter Supplier ID (or type 'cancel' to exit): ");
-            String supplierID = scanner.nextLine().trim();
-            if (supplierID.equalsIgnoreCase("cancel")) return;
-            Supplier supplier = supplierManager.getSupplier(supplierID);
-            if (supplier != null) {
-                BusinessOrder order = new BusinessOrder(supplier, report, inventory);
-                while (true) {
-                    List<Product> products = inventory.getProductsBySupplier(supplier);
-                    System.out.println("The available products:");
-                    for (Product product : products) {
-                        System.out.println(product.toString());
-                    }
+        System.out.println("\n--- Supplier Order Menu ---");
+        System.out.println("Enter Supplier ID (or type 'cancel' to exit): ");
+        String supplierID = scanner.nextLine().trim();
+        if (supplierID.equalsIgnoreCase("cancel")) return;
+        Supplier supplier = supplierManager.getSupplier(supplierID);
+        if (supplier != null) {
+            BusinessOrder order = new BusinessOrder(supplier, report, inventory);
+            while (true) {
+                List<Product> products = inventory.getProductsBySupplier(supplier);
+                System.out.println("The available products:");
+                for (Product product : products) {
+                    System.out.println(product.toString());
+                }
 
-                    System.out.println("Current Order: ");
-                    order.currentOrder();
-                    System.out.println("Enter Product ID  to add a product (or type 'cancel' to exit, or 'complete' to complete the order): ");
-                    String productID = scanner.nextLine().trim();
-                    if (productID.equalsIgnoreCase("cancel")) return;
-                    if (productID.equalsIgnoreCase("complete")) {
-                        order.completeOrder();
-                        System.out.println("The order has been completed.");
-                        break;
-                    }
-                    Product product = inventory.getProductById(productID);
-                    int currentQuantity = order.currentQuantity(product);
+                System.out.println("Current Order: ");
+                order.currentOrder();
+                System.out.println("Enter Product ID  to add a product (or type 'cancel' to exit, or 'complete' to complete the order): ");
+                String productID = scanner.nextLine().trim();
+                if (productID.equalsIgnoreCase("cancel")) return;
+                if (productID.equalsIgnoreCase("complete")) {
+                    order.completeOrder();
+                    orderManager.addOrder(order);
+                    System.out.println("The order has been completed.");
+                    break;
+                }
+                Product product = inventory.getProductById(productID);
+                int currentQuantity = order.currentQuantity(product);
+                if (product != null) {
+
                     if (currentQuantity != 0) {
                         System.out.println("This product is already in the basket. The current order quantity is: " + order.getOrderedProducts());
                         System.out.println("How many would you like to order in TOTAL? If there are currently 4 products, and you enter 2. The basket will contain only 2 of that item");
                         System.out.println("Enter '0' to remove the product from the order");
                         System.out.print("Enter choice: ");
-                        int quantity = Integer.parseInt(scanner.nextLine().trim());;
+                        int quantity = Integer.parseInt(scanner.nextLine().trim());
+                        ;
                         if (quantity == 0) {
                             order.removeItem(productID);
                         } else {
                             order.changeProductQuantity(product, quantity);
                         }
-                    }
-                    if (product != null) {
+                    } else {
                         System.out.println("Product: " + product.getName());
                         System.out.println("Your current stock level is: " + product.getQuantity());
                         System.out.println("How many would you like to add? (type '0' to cancel ordering this product): ");
                         int quantity = Integer.parseInt(scanner.nextLine().trim());
                         if (quantity == 0) return;
                         order.addItem(productID, quantity);
-                    } else {
-                        System.out.println("Product does not exist with ID: " + productID);
                     }
+                } else {
+                    System.out.println("Product does not exist with ID: " + productID);
                 }
-            } else {
-                System.out.println("No supplier with ID: " + supplierID);
             }
-
+        } else {
+            System.out.println("No supplier with ID: " + supplierID);
         }
     }
 
@@ -434,45 +435,45 @@ public class Main {
     }
 
     private static void orderFromWarehouse() {
+        System.out.println("\n--- Product Order Menu ---");
+        CustomerOrder order = new CustomerOrder(report, inventory);
         while (true) {
-            System.out.println("\n--- Product Order Menu ---");
-            CustomerOrder order = new CustomerOrder(report, inventory);
-            while (true) {
-                inventory.listProducts();
-                System.out.println("Current Order: ");
-                order.currentOrder();
-                System.out.println("Enter Product ID  to add a product (or type 'cancel' to exit, or 'complete' to complete the order): ");
-                String productID = scanner.nextLine().trim();
-                if (productID.equalsIgnoreCase("cancel")) return;
-                if (productID.equalsIgnoreCase("complete")) {
-                    order.completeOrder();
-                    System.out.println("The order has been completed.");
-                    break;
-                }
-                Product product = inventory.getProductById(productID);
-                int currentQuantity = order.currentQuantity(product);
-                if (currentQuantity != 0) {
-                    System.out.println("This product is already in the basket. The current order quantity is: " + order.getOrderedProducts());
-                    System.out.println("How many would you like to order in TOTAL? If there are currently 4 products, and you enter 2. The basket will contain only 2 of that item");
-                    System.out.println("Enter '0' to remove the product from the order");
-                    System.out.print("Enter choice: ");
-                    int quantity = Integer.parseInt(scanner.nextLine().trim());;
-                    if (quantity == 0) {
-                        order.removeItem(productID);
-                    } else {
-                        order.changeProductQuantity(product, quantity);
-                    }
-                }
-                if (product != null) {
-                    System.out.println("Product: " + product.getName());
-                    System.out.println("Your current stock level is: " + product.getQuantity());
-                    System.out.println("How many would you like to add? (type '0' to cancel ordering this product): ");
-                    int quantity = Integer.parseInt(scanner.nextLine().trim());
-                    if (quantity == 0) return;
-                    order.addItem(productID, quantity);
+            inventory.listProducts();
+            System.out.println("Current Order: ");
+            order.currentOrder();
+            System.out.println("Enter Product ID  to add a product (or type 'cancel' to exit, or 'complete' to complete the order): ");
+            String productID = scanner.nextLine().trim();
+            if (productID.equalsIgnoreCase("cancel")) return;
+            if (productID.equalsIgnoreCase("complete")) {
+                order.completeOrder();
+                orderManager.addOrder(order);
+                System.out.println("The order has been completed.");
+                break;
+            }
+            Product product = inventory.getProductById(productID);
+            int currentQuantity = order.currentQuantity(product);
+            if (currentQuantity != 0) {
+                System.out.println("This product is already in the basket. The current order quantity is: " + order.getOrderedProducts());
+                System.out.println("How many would you like to order in TOTAL? If there are currently 4 products, and you enter 2. The basket will contain only 2 of that item");
+                System.out.println("Enter '0' to remove the product from the order");
+                System.out.print("Enter choice: ");
+                int quantity = Integer.parseInt(scanner.nextLine().trim());
+                ;
+                if (quantity == 0) {
+                    order.removeItem(productID);
                 } else {
-                    System.out.println("Product does not exist with ID: " + productID);
+                    order.changeProductQuantity(product, quantity);
                 }
+            }
+            if (product != null) {
+                System.out.println("Product: " + product.getName());
+                System.out.println("Your current stock level is: " + product.getQuantity());
+                System.out.println("How many would you like to add? (type '0' to cancel ordering this product): ");
+                int quantity = Integer.parseInt(scanner.nextLine().trim());
+                if (quantity == 0) return;
+                order.addItem(productID, quantity);
+            } else {
+                System.out.println("Product does not exist with ID: " + productID);
             }
         }
     }
--- src/main/order/OrderManager.java
+++ src/main/order/OrderManager.java
@@ -1,9 +1,5 @@
 package main.order;
 
-import main.financial.FinancialReport;
-import main.products.InventoryManager;
-import main.suppliers.Supplier;
-
 import java.util.ArrayList;
 import java.util.List;
 
@@ -15,24 +11,14 @@ public class OrderManager {
         this.orders = new ArrayList<BaseOrder>();
     }
 
-    public void createOrder(Supplier supplier, FinancialReport report, InventoryManager inventory) {
-        BusinessOrder order = new BusinessOrder(supplier, report, inventory);
+    public void addOrder(BaseOrder order) {
         orders.add(order);
-    }
-
-    public BaseOrder getOrderById(String id) {
-        for (BaseOrder order : orders) {
-            if (order.getOrderID().equals(id)) {
-                return order;
-            }
-        }
-        return null;
     }
 
     public void displayCustomerOrders() {
         for (BaseOrder order : orders) {
             if (order instanceof CustomerOrder) {
-                printOrders((CustomerOrder) order);
+                printOrders(order);
             }
         }
     }
@@ -40,13 +26,13 @@ public class OrderManager {
     public void displayBusinessOrders() {
         for (BaseOrder order : orders) {
             if (order instanceof BusinessOrder) {
-                printOrders((BusinessOrder) order);
+                printOrders(order);
             }
         }
     }
 
     private void printOrders(BaseOrder order) {
-        System.out.println("Order ID: " + order.getOrderID() + "Order Status: " + order.getOrderStatus() + "Order Date: " + order.getOrderDate());
+        System.out.println("Order ID: " + order.getOrderID() + ", Order Status: " + order.getOrderStatus() + ", Order Date: " + order.getOrderDate());
         if (order.getOrderedProducts().isEmpty()) {
             System.out.println("This order is empty");
         } else {
@@ -61,11 +47,12 @@ public class OrderManager {
         for (BaseOrder order : orders) {
             if (order instanceof BusinessOrder) {
                 if (((BusinessOrder) order).isArrived()) {
-                    arrivalOrders.add((BusinessOrder) order);
+                    if (!order.isDelivered()) {
+                        arrivalOrders.add((BusinessOrder) order);
+                    }
                 }
             }
         }
         return arrivalOrders;
     }
-
 }
